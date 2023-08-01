5a3882ec880328c900c765708d530c8fdb6ad3d2
AlishaY <36836010+AlishaY@users.noreply.github.com>
2023-08-01 14:56:45 +0800

    ✅ success insert into database

--- controller/controller.go
+++ controller/controller.go
@@ -6,7 +6,7 @@ import (
 	// "time"
 	// "fmt"
 	// "io/ioutil"
-	// "net/http"
+	"net/http"
 	// "path/filepath"
 	// "os"
 	"log"
@@ -74,17 +74,12 @@ func (c *KPIController) PostFile(ctx *gin.Context) {
 
 	// Flags to track the presence of "Criteria" and "Performance Grading"
 	criteriaFound,weightageFound, individualFound, totalFound := false, false, false, false
-	// weightageFound := false
-	// individualFound := false
-	// totalFound := false
 
 	gradingFound := false
 	grading1, grading2, grading3, grading4 := false, false, false, false
 
 	kraFound := false
 	taskFound := false
-
-	// taskType, behaviorType, organizationalType, individualType := false, false, false, false
 
 	// Iterate through the rows and store the values in the array
 	for rowIndex, row := range rows {
@@ -155,7 +150,6 @@ func (c *KPIController) PostFile(ctx *gin.Context) {
 		log.Println("NOT FOUND")
 	}
 
-	// Create arrays to store the extracted data
 	var dataTask [][]string
 	var dataBehavior [][]string
 	var dataOrg [][]string
@@ -166,7 +160,6 @@ func (c *KPIController) PostFile(ctx *gin.Context) {
 	if headerComplete {
 		log.Println("Header of the file is Complete. Continue reading data...")
 
-		// Flags to track the start and end of data sections
 		var readA bool
 		var readB bool
 		var readC bool
@@ -174,187 +167,137 @@ func (c *KPIController) PostFile(ctx *gin.Context) {
 
 		// Iterate through the rows and store the values in the array
 		for rowIndex, row := range rows {
-    	if rowIndex >= 2 {
-        	if len(row) > 0 && row[0] == "A" {
-            	// Skip reading the current row if a cell in column A is equal to "A"
-            	readA = true
-            	continue
-        	}
+			if rowIndex >= 2 {
+				if len(row) > 0 && row[0] == "A" {
+					// Skip reading the current row if a cell in column A is equal to "A"
+					readA = true
+					continue
+				}
+				if readA && !readB {
+					// Create an array to store the extracted data for the current row
+					var rowData []string
 
-        	if readA && !readB {
-            // Create an array to store the extracted data for the current row
-            var rowData []string
+					for _, cell := range row {
+						// Check if the cell is not empty
+						if cell != "" {
+							// Check if the cell value is "B"
+							if cell == "B" {
+								// Mark the end of the "dataTask" section and start the "dataBehavior" section
+								readA = false
+								readB = true
+								break
+							}
+							if cell == "#REF!" {
+								ctx.JSON(400, gin.H{"message": "Upload fail"})
+								return
+							}
+							rowData = append(rowData, cell)
+						}
+					}
+					rowDataString := strings.Join(rowData, ", ")
 
-            for _, cell := range row {
-                // Check if the cell is not empty
-                if cell != "" {
-                    // Check if the cell value is "B"
-                    if cell == "B" {
-                        // Mark the end of the "dataTask" section and start the "dataBehavior" section
-                        readA = false
-                        readB = true
-                        break
-                    }
+					if rowDataString != "" {
+						rowData = append(rowData, "Task")
+						dataTask = append(dataTask, rowData)
+						fmt.Printf("Extracted Data for Row %d (dataTask): ----- [ %s ] -----\n", rowIndex+1, dataTask)
+					}
+				} else if readB && !readC {
+					// Create an array to store the extracted data for the current row
+					var rowData []string
 
-                    // Check if the cell value is "#REF!"
-                    if cell == "#REF!" {
-                        ctx.JSON(400, gin.H{"message": "Upload fail"})
-                        return
-                    }
+					for _, cell := range row {
+						// Check if the cell is not empty
+						if cell != "" {
+							// Check if the cell value is "C"
+							if cell == "C" {
+								// Mark the end of the "dataBehavior" section and start the "dataOrg" section
+								readB = false
+								readC = true
+								break
+							}
+							// Check if the cell value is "#REF!"
+							if cell == "#REF!" {
+								ctx.JSON(400, gin.H{"message": "Upload fail"})
+								return
+							}
+							// Store the cell value in the dataBehavior array
+							rowData = append(rowData, cell)
+						}
+					}
+					rowDataString := strings.Join(rowData, ", ")
 
-                    // Store the cell value in the dataTask array
-                    rowData = append(rowData, cell)
+					if rowDataString != "" {
+						rowData = append(rowData, "Behavior/Attitude")
+						dataBehavior = append(dataBehavior, rowData)
+						fmt.Printf("Extracted Data for Row %d (dataBehavior): ----- [ %s ] -----\n", rowIndex+1, dataBehavior)
+					}
+				} else if readC && !readD {
+					// Create an array to store the extracted data for the current row
+					var rowData []string
 
-                }
-            }
+					for _, cell := range row {
+						// Check if the cell is not empty
+						if cell != "" {
+							// Check if the cell value is "D"
+							if cell == "D" {
+								// Mark the end of the "dataOrg" section and start the "dataIndividual" section
+								readC = false
+								readD = true
+								break
+							}
+							// Check if the cell value is "#REF!"
+							if cell == "#REF!" {
+								ctx.JSON(400, gin.H{"message": "Upload fail"})
+								return
+							}
+							// Store the cell value in the dataOrg array
+							rowData = append(rowData, cell)
+						}
+					}
+					rowDataString := strings.Join(rowData, ", ")
 
-            // Print the rowData for the current row
-            rowDataString := strings.Join(rowData, ", ")
+					if rowDataString != "" {
+						rowData = append(rowData, "Organizational Goal")
+						dataOrg = append(dataOrg, rowData)
+						fmt.Printf("Extracted Data for Row %d (dataOrg): ----- [ %s ] -----\n", rowIndex+1, dataOrg)
+					}
+				} else if readD {
+					// Create an array to store the extracted data for the current row
+					var rowData []string
 
-            if rowDataString != "" {
-                // fmt.Printf("Extracted Data for Row %d (dataTask): ----- [ %s ] -----\n", rowIndex+1, rowDataString)
-				// fmt.Println("LENGTH OF THE ROW: -------------", rowDataString[9])
+					for _, cell := range row {
+						// Check if the cell is not empty
+						if cell != "" {
+							// Check if the cell value is "#REF!"
+							if cell == "#REF!" {
+								ctx.JSON(400, gin.H{"message": "Upload fail"})
+								return
+							}
+							// Store the cell value in the dataIndividual array
+							rowData = append(rowData, cell)
+						}
+					}
+					rowDataString := strings.Join(rowData, ", ")
 
-                // Store the rowDataString in the dataTask array
-                // dataTask = append(dataTask, rowDataString, "Task")
-				// Append "Task" at the end of the rowData
-				rowData = append(rowData, "Task")
-
-				// Store the rowData in the dataTask array
-				dataTask = append(dataTask, rowData)
-				// Print the modified dataTask
-				fmt.Printf("Extracted Data for Row %d (dataTask): ----- [ %s ] -----\n", rowIndex+1, dataTask)
-				dataAll = append(dataAll, dataTask...)
-            }
-        } else if readB && !readC {
-            // Create an array to store the extracted data for the current row
-            var rowData []string
-
-            for _, cell := range row {
-                // Check if the cell is not empty
-                if cell != "" {
-                    // Check if the cell value is "C"
-                    if cell == "C" {
-                        // Mark the end of the "dataBehavior" section and start the "dataOrg" section
-                        readB = false
-                        readC = true
-                        break
-                    }
-
-                    // Check if the cell value is "#REF!"
-                    if cell == "#REF!" {
-                        ctx.JSON(400, gin.H{"message": "Upload fail"})
-                        return
-                    }
-
-                    // Store the cell value in the dataBehavior array
-                    rowData = append(rowData, cell)
-                }
-            }
-
-            // Print the rowData for the current row
-            rowDataString := strings.Join(rowData, ", ")
-
-            if rowDataString != "" {
-                // fmt.Printf("Extracted Data for Row %d (dataBehavior): ----- [ %s ] -----\n", rowIndex+1, rowDataString)
-                // Store the rowDataString in the dataBehavior array
-                rowData = append(rowData, "Behavior/Attitude")
-
-				// Store the rowData in the dataBehavior array
-				dataBehavior = append(dataBehavior, rowData)
-				// Print the modified dataBehavior
-				fmt.Printf("Extracted Data for Row %d (dataBehavior): ----- [ %s ] -----\n", rowIndex+1, dataBehavior)
-				dataAll = append(dataAll, dataBehavior...)
-            }
-        } else if readC && !readD {
-            // Create an array to store the extracted data for the current row
-            var rowData []string
-
-            for _, cell := range row {
-                // Check if the cell is not empty
-                if cell != "" {
-                    // Check if the cell value is "D"
-                    if cell == "D" {
-                        // Mark the end of the "dataOrg" section and start the "dataIndividual" section
-                        readC = false
-                        readD = true
-                        break
-                    }
-
-                    // Check if the cell value is "#REF!"
-                    if cell == "#REF!" {
-                        ctx.JSON(400, gin.H{"message": "Upload fail"})
-                        return
-                    }
-
-                    // Store the cell value in the dataOrg array
-                    rowData = append(rowData, cell)
-                }
-            }
-
-            // Print the rowData for the current row
-            rowDataString := strings.Join(rowData, ", ")
-
-            if rowDataString != "" {
-                rowData = append(rowData, "Organizational Goal")
-
-				// Store the rowData in the dataOrg array
-				dataOrg = append(dataOrg, rowData)
-				// Print the modified dataOrg
-				fmt.Printf("Extracted Data for Row %d (dataOrg): ----- [ %s ] -----\n", rowIndex+1, dataOrg)
-				dataAll = append(dataAll, dataOrg...)
-            }
-        } else if readD {
-            // Create an array to store the extracted data for the current row
-            var rowData []string
-
-            for _, cell := range row {
-                // Check if the cell is not empty
-                if cell != "" {
-                    // Check if the cell value is "#REF!"
-                    if cell == "#REF!" {
-                        ctx.JSON(400, gin.H{"message": "Upload fail"})
-                        return
-                    }
-
-                    // Store the cell value in the dataIndividual array
-                    rowData = append(rowData, cell)
-                }
-            }
-
-            // Print the rowData for the current row
-            rowDataString := strings.Join(rowData, ", ")
-
-            if rowDataString != "" {
-                // fmt.Printf("Extracted Data for Row %d (dataIndividual): ----- [ %s ] -----\n", rowIndex+1, rowDataString)
-                // Store the rowDataString in the dataIndividual array
-                rowData = append(rowData, "Organizational Goal")
-
-				// Store the rowData in the dataIndividual array
-				dataIndividual = append(dataIndividual, rowData)
-				// Print the modified dataIndividual
-				fmt.Printf("Extracted Data for Row %d (dataIndividual): ----- [ %s ] -----\n", rowIndex+1, dataIndividual)
-				dataAll = append(dataAll, dataIndividual...)
-            }
-        }
-    }
-	}
+					if rowDataString != "" {
+						rowData = append(rowData, "Individual Goal")
+						dataIndividual = append(dataIndividual, rowData)
+						fmt.Printf("Extracted Data for Row %d (dataIndividual): ----- [ %s ] -----\n", rowIndex+1, dataIndividual)
+					}
+				}
+			}
+		}
+		dataAll = append(dataAll, dataTask...)
+		dataAll = append(dataAll, dataBehavior...)
+		dataAll = append(dataAll, dataOrg...)
+		dataAll = append(dataAll, dataIndividual...)
 	} else {
 		log.Println("Header of the file is NOT COMPLETE.")
 	}
-	// dataTaskString := strings.Join(dataTask, "|")
 
-	// fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@THIS THE SET OBJECTIVE", dataAll)
-	// Create an array to store the parsed JSON data
-	var kpiDataJSON []KpiFileJson
+	jsonData := make([]map[string]interface{}, 0)
 
-	// Parse the dataOrg array into instances of the KpiFileJson struct
-for _, kpiValues := range dataAll {
-    // Trim the square brackets from the string
-    // kpiRow = strings.TrimSuffix(strings.TrimPrefix(kpiRow, "["), "]")
-
-    // // Split the string by commas
-    // kpiValues := strings.Split(kpiRow, ", ")
+	for _, kpiValues := range dataAll {
 		removePercent := strings.Replace(kpiValues[3], "%", "", 1)
 		// Convert the number value to an integer
 		numberValue, err := strconv.Atoi(removePercent)
@@ -365,16 +308,6 @@ for _, kpiValues := range dataAll {
 
 		loginName := ctx.PostForm("loginName")
 		period := ctx.PostForm("period")
-		// objectiveType := ctx.PostForm("objectiveType")
-		// objectiveTypeStr := ctx.PostForm("objectiveType")
-        // objectiveType, err := strconv.Atoi(objectiveTypeStr)
-        // if err != nil {
-        //     log.Printf("Error converting objectiveType to integer: %v", err)
-        //     // Return a JSON response with the error message and set the status code to 400 (Bad Request)
-        //     ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid objectiveType"})
-        //     return
-        // }
-    	// Check if kpiValues has at least 9 elements (0 to 8 indexes)
     	if len(kpiValues) >= 10 {
         // Create an instance of the KpiFileJson struct
         	data := KpiFileJson{
@@ -390,38 +323,36 @@ for _, kpiValues := range dataAll {
 				Mark4Desc:          kpiValues[8],
         	}
 
-        // Save the data to the database using the SaveToDatabase function
-        if err := c.SaveToDatabase(data); err != nil {
-            log.Printf("Error saving data to database: %v", err)
-            // Handle the error (e.g., return an error response or take appropriate action)
-            // ...
-			// ctx.JSON(500, gin.H{"message": "Inserting KPI data failed."})
-			// fmt.Println("ERROR INSERTING DATA TO DATABASE")
-			// return
-        }
-    } else {
-        // Log the data that caused the issue for debugging
-        log.Printf("kpiValues does not have enough elements: %+v", kpiValues)
-    }
-}
+			// Convert data to a map of key-value pairs based on the KpiFileJson struct
+            dataMap := map[string]interface{}{
+                "nameId":             data.NameId,
+                "period":             data.Period,
+                "objType":            data.ObjectiveType,
+                "kra":                data.KRA,
+                "desc":               data.Description,
+                "individualCriteria": data.IndividualCriteria,
+                "mark1Desc":          data.Mark1Desc,
+                "mark2Desc":          data.Mark2Desc,
+                "mark3Desc":          data.Mark3Desc,
+                "mark4Desc":          data.Mark4Desc,
+            }
 
-	// Convert kpiDataJSON to JSON
-	// jsonData, err := json.Marshal(kpiDataJSON)
-	// if err != nil {
-	// 	log.Println("Error marshaling JSON:", err)
-	// 	return
-	// }
-
-	// Print the JSON data
-	// fmt.Println(string(jsonData))
+			// Save the data to the database using the SaveToDatabase function
+			if err := c.SaveToDatabase(data); err != nil {
+				log.Printf("Error saving data to database: %v", err)
+				// return
+			}
+			// Append the dataMap to the jsonData slice
+			jsonData = append(jsonData, dataMap)
+		} else {
+			// Log the data that caused the issue for debugging
+			log.Printf("kpiValues does not have enough elements: %+v", kpiValues)
+		}
+	}
 	fmt.Println("++++++Data from ALL DATA array:\n\n\n", dataAll)
-
-	// ctx.JSON(200, gin.H{"message": "File uploaded and processed successfully"})
-	// Send the JSON response back to the client (Postman)
-	ctx.JSON(200, kpiDataJSON)
+	ctx.JSON(http.StatusOK, jsonData)
 }
 
-// SaveToDatabase inserts the data into the "Criteria" table using Exec.
 func (c *KPIController) SaveToDatabase(data KpiFileJson) error {
     // Ensure the database connection is open
     if c.db.Error != nil {
